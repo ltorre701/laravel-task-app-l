96e2e5075d61ce6055c9ba4044bf53f160834420
Luis Torres <142362616+ltorre701@users.noreply.github.com>
2025-02-21 22:21:44 +0000

    Updated task controller for validations

--- taskApp/app/Http/Controllers/TaskController.php
+++ taskApp/app/Http/Controllers/TaskController.php
@@ -56,17 +56,8 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'task_name' => 'required|string|max:255',
-            'task_location' => 'nullable|string|max:255',
-            'time_complexity' => 'required|integer|min:1|max:5',
-            'materials_required' => 'nullable|string',
-            'deadline' => 'nullable|date',
-            'priority' => 'nullable|integer|min:1|max:3',
-            'category' => 'nullable|string|max:255',
-        ]);
-
-        task::create($validated);
+        $validated = $request->validate(Task::validationRules());
+        Task::create($validated);
         return redirect()->route('tasks.index')->with('success', 'Task created successfully!');
     }
 
@@ -93,19 +84,9 @@ class TaskController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $validated = $request->validate([
-            'task_name' => 'required|string|max:255',
-            'task_location' => 'nullable|string|max:255',
-            'time_complexity' => 'required|integer|min:1|max:255',
-            'materials_required' => 'nullable|string',
-            'deadline' => 'nullable|date',
-            'priority' => 'nullable|integer|min:1|max:3',
-            'category' => 'nullable|string|max:255',
-        ]);
-
+        $validated = $request->validate(Task::validationRules());
         $task = Task::findOrFail($id);
         $task->update($validated);
-
         return redirect()->route('tasks.index')->with('success', 'Task updated successfully!');
     }
 
