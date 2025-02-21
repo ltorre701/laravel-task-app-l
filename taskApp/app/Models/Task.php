d9be2d7dff389d13a638d0b086e12c1568422fb0
Luis Torres <142362616+ltorre701@users.noreply.github.com>
2025-02-21 22:18:09 +0000

    created backend validation

--- taskApp/app/Models/Task.php
+++ taskApp/app/Models/Task.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Models;
-
+use Illuminate\Validation\Rule;
 use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
@@ -15,4 +15,44 @@ class Task extends Model
         'priority',
         'category'
     ];
+
+    public static function validationRules()
+    {
+        return [
+            'task_name' => [
+                'required',
+                'string',
+                'max:255',
+                'regex:/^[a-zA-Z0-9\s\-_,]{0,255}$/'
+            ],
+            'time_complexity' => [
+                'required',
+                'integer',
+                'min:1',
+                'max:5'
+            ],
+            'materials_required' => [
+                'nullable',
+                'string',
+                'regex:^[\w\s,.-]{0,1000}$/'
+            ],
+            'deadline' => [
+                'nullable',
+                'date',
+                'after:today'
+            ],
+            'priority' => [
+                'nullable',
+                'integer',
+                'min:1',
+                'max:3'
+            ],
+            'category' => [
+                'nullable',
+                'string',
+                'max:50',
+                'regex:/^[a-zA-Z\s]{0-50}$/'
+            ],
+        ];
+    }
 }
